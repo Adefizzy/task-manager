6c84da68ef999fecec3969105cb43d4d1980ef21
adefisayo adeniji <adefizzy045@yahoo.com>
2024-07-08 07:37:03 +0100

    added exception filters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,66 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config, Config } from './config';
+import { Logger } from 'nestjs-pino';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const appConfig: Config = config;
+  const PORT = config.server.port;
+
+  NestFactory.create(AppModule.forRoot({ config: appConfig }))
+    .then((app) => {
+      const logger = app.get(Logger);
+      app.useLogger(logger);
+      app.enableCors({
+        origin: ['http://localhost:8888'],
+        allowedHeaders: ['content-type', 'Authorization'],
+        credentials: true,
+      });
+
+      app.use(helmet());
+      app.use(cookieParser());
+      app.setGlobalPrefix('api/v1');
+
+      const config = new DocumentBuilder()
+        .setTitle('enif-user-taskmanager api docs')
+        .setDescription('user task manager api docs')
+        .setVersion('1.0')
+        .addBearerAuth(
+          {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'Token',
+          },
+          'access-token',
+        )
+        .build();
+
+      const options: SwaggerDocumentOptions = {
+        operationIdFactory: (controllerKey: string, methodKey: string) =>
+          methodKey,
+      };
+
+      const document = SwaggerModule.createDocument(app, config, options);
+      SwaggerModule.setup('api', app, document);
+
+      app
+        .listen(PORT)
+        .then(() => {
+          logger.log(`⚡️[server]: Server is running at port ${PORT}`);
+        })
+        .catch((error) => {
+          logger.error(`error listening on ${PORT}`, { error });
+        });
+    })
+    .catch((error) => {
+      console.log('error starting app', JSON.stringify(error));
+    });
 }
 bootstrap();
